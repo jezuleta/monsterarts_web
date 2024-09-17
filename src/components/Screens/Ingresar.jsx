13a24b1b0258339e4010449c73c2f3b1c9406b13
jezuleta <50642495+jezuleta@users.noreply.github.com>
2024-09-16 23:10:37 -0500

    Ingresar / inicio

--- src/components/Screens/Ingresar.jsx
+++ src/components/Screens/Ingresar.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import style from './Ingresar.module.css'
+//import React from 'react';
+import style from './Ingresar.module.css';
+import { useForm } from "react-hook-form";
 
 const Ingresar = () => {
+
+  const { register, handleSubmit} = useForm();
+
   return (
     <div className={style.principal}>
       
       <div className={style.box}>
-        <h1>Ingresar</h1>
-        
+        <h1 className={style.title} >Ingresar</h1>
+        <form action="">
+          <div>
+            <label>Correo</label>
+            <imput type="text" name="" />
+          </div>
+          <div>
+            <label>Constraseña</label>
+            <imput type="password" />
+          </div>
+        </form>
       </div>
 
     </div>
